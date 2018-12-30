eb577343a576fb8a3120ff8c0700bb65cabe35b5
ghetto <ghetto.ch@gmail.com>
2018-12-30 14:33:50 +0100

    new theme

--- init.el
+++ init.el
@@ -24,9 +24,43 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#3F3F3F" "#CC9393" "#7F9F7F" "#F0DFAF" "#8CD0D3" "#DC8CC3" "#93E0E3" "#DCDCCC"])
+ '(company-quickhelp-color-background "#4F4F4F")
+ '(company-quickhelp-color-foreground "#DCDCCC")
+ '(custom-safe-themes
+   (quote
+    ("16dd114a84d0aeccc5ad6fd64752a11ea2e841e3853234f19dc02a7b91f5d661" default)))
+ '(fci-rule-color "#383838")
+ '(nrepl-message-colors
+   (quote
+    ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
  '(package-selected-packages
    (quote
-    (pandoc-mode webpaste helm-pass immortal-scratch cheatsheet company-quickhelp window-purpose ibuffer-projectile all-the-icons-dired dired-hide-dotfiles dired-collapse ranger pdf-tools helm-ag helm-projectile exec-path-from-shell diminish ox-twbs org-bullets htmlize sudo-edit magit paredit geiser company-shell slime-company slime pyenv-mode py-autopep8 company-irony company-c-headers flycheck-clang-analyzer platformio-mode highlight-indent-guides company flycheck yasnippet-snippets yasnippet expand-region rainbow-delimiters rainbow-mode multiple-cursors avy helm-swoop helm which-key symon spaceline projectile async zenburn-theme use-package))))
+    (base16-theme pandoc-mode webpaste helm-pass immortal-scratch cheatsheet company-quickhelp window-purpose ibuffer-projectile all-the-icons-dired dired-hide-dotfiles dired-collapse ranger pdf-tools helm-ag helm-projectile exec-path-from-shell diminish ox-twbs org-bullets htmlize sudo-edit magit paredit geiser company-shell slime-company slime pyenv-mode py-autopep8 company-irony company-c-headers flycheck-clang-analyzer platformio-mode highlight-indent-guides company flycheck yasnippet-snippets yasnippet expand-region rainbow-delimiters rainbow-mode multiple-cursors avy helm-swoop helm which-key symon spaceline projectile async zenburn-theme use-package)))
+ '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
+ '(vc-annotate-background "#2B2B2B")
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#BC8383")
+     (40 . "#CC9393")
+     (60 . "#DFAF8F")
+     (80 . "#D0BF8F")
+     (100 . "#E0CF9F")
+     (120 . "#F0DFAF")
+     (140 . "#5F7F5F")
+     (160 . "#7F9F7F")
+     (180 . "#8FB28F")
+     (200 . "#9FC59F")
+     (220 . "#AFD8AF")
+     (240 . "#BFEBBF")
+     (260 . "#93E0E3")
+     (280 . "#6CA0A3")
+     (300 . "#7CB8BB")
+     (320 . "#8CD0D3")
+     (340 . "#94BFF3")
+     (360 . "#DC8CC3"))))
+ '(vc-annotate-very-old-color "#DC8CC3"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
